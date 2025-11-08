98d893a7addb79083346fa30e1885dc342fe2e62
cji <gugugeul273@gmail.com>
2025-11-08 14:15:56 +0900

    feat(zustand):actions 객체 활용으로 한 번에 불러오기

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -1,15 +1,14 @@
 import { useCountStore } from "@/store/count";
 import { Button } from "@/components/ui/button";
 export default function Controller() {
-  const decrease = useCountStore((store) => store.decrease);
-  const increase = useCountStore((store) => store.increase);
+  const actions = useCountStore((store) => store.actions);
   //   const store = useCountStore(); 를 구조분해한 결과
 
   return (
     <>
       {/* <Button onClick={decrease}>-</Button> */}
-      <Button onClick={decrease}>-</Button>
-      <Button onClick={increase}>+</Button>
+      <Button onClick={actions.decrease}>-</Button>
+      <Button onClick={actions.increase}>+</Button>
     </>
   );
 }

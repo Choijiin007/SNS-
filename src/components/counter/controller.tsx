7e0562fa55fca419366813eac62a6955f80d085f
cji <gugugeul273@gmail.com>
2025-11-08 14:36:10 +0900

    feat(zustand):커스텀 훅으로 useCountStore() 불러오기

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -1,14 +1,15 @@
-import { useCountStore } from "@/store/count";
+import { usedecreasCount, useIncreaseCount } from "@/store/count";
 import { Button } from "@/components/ui/button";
 export default function Controller() {
-  const actions = useCountStore((store) => store.actions);
-  //   const store = useCountStore(); 를 구조분해한 결과
+  // 변수가 달라질 경우를 대비하여 커스텀 훅으로 변경
+  const increase = useIncreaseCount();
+  const decrease = usedecreasCount();
 
   return (
     <>
       {/* <Button onClick={decrease}>-</Button> */}
-      <Button onClick={actions.decrease}>-</Button>
-      <Button onClick={actions.increase}>+</Button>
+      <Button onClick={decrease}>-</Button>
+      <Button onClick={increase}>+</Button>
     </>
   );
 }
--- src/store/count.ts
+++ src/store/count.ts
@@ -4,20 +4,34 @@ import { create } from "zustand";
 
 type Store = {
   count: number;
-  increase: () => void;
-  decrease: () => void;
+  actions: { increaseOne: () => void; decreaseOne: () => void };
 };
 //create는 값을 반환하기 까지 함. -> hook으로 사용 가능
 export const useCountStore = create<Store>((set, get) => ({
   count: 0,
-  increase: () => {
-    set((store) => ({
-      count: store.count + 1,
-    }));
-  },
-  decrease: () => {
-    set((store) => ({
-      count: store.count - 1,
-    }));
+  actions: {
+    increaseOne: () => {
+      set((store) => ({
+        count: store.count + 1,
+      }));
+    },
+    decreaseOne: () => {
+      set((store) => ({
+        count: store.count - 1,
+      }));
+    },
   },
 })); //return문 내부가 create함수가 생성하는 스토어이다.));
+
+export const useCount = () => {
+  const count = useCountStore((store) => store.count);
+  return count;
+};
+export const usedecreasCount = () => {
+  const decrease = useCountStore((store) => store.actions.decreaseOne);
+  return decrease;
+};
+export const useIncreaseCount = () => {
+  const increase = useCountStore((store) => store.actions.increaseOne);
+  return increase;
+};
